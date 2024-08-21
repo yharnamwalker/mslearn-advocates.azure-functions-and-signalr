d2401df61b766104177e609fb4fa6d004dc9bae4
Ricky <ricky.schuleman@property.nhs.uk>
2024-08-21 15:06:49 +0100

    test

--- start/client/webpack.config.js
+++ start/client/webpack.config.js
@@ -2,59 +2,66 @@ const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
-module.exports = {
+module.exports = (env) => {
 
+  console.log('env: ', env)
+  console.log('process.env: ', process.env)
+
+  return {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
     },
+    stats: 'verbose',
     devServer: {
-        static: {
-          directory: path.join(__dirname, 'dist'),
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 3000,
+      allowedHosts: 'all',
+      client: {
+        logging: 'verbose',
+        overlay: true,
+      },
+      proxy: [
+        {
+          context: ['/api'],
+          target: process.env.BACKEND_URL || 'http://localhost:7071',
         },
-        compress: true,
-        port: 3000,
-        allowedHosts: 'all',
-        client: {
-            logging: 'verbose',
-            overlay: true,
-          },
-          proxy: [
-            {
-              context: ['/api'],
-              target: 'http://localhost:7071',
-            },
-          ],
-      },
+      ],
+    },
     module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            "test": /\.js$/,
-            "exclude": /node_modules/,
-            "use": {
-              "loader": "babel-loader",
-              "options": {
-                "presets": [
-                  "@babel/preset-env",
-                ]
-              }
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          "test": /\.js$/,
+          "exclude": /node_modules/,
+          "use": {
+            "loader": "babel-loader",
+            "options": {
+              "presets": [
+                "@babel/preset-env",
+              ]
             }
+          }
         }
-        ]
-      },
-      plugins: [
-        new Dotenv(),
-        new CopyWebpackPlugin({
-            patterns: [
-              { from: './src/favicon.ico', to: './' },
-              { from: './index.html', to: './'}
-            ],
-          }),
+      ]
+    },
+    plugins: [
+      new Dotenv({
+        systemvars: true
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './src/favicon.ico', to: './' },
+          { from: './index.html', to: './' }
+        ],
+      })
     ]
-      
+  }
 }
